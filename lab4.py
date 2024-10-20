51ebb2007e1f7948d09c12c8b83e521152fd8ec6
jmoy <jmoy02@calpoly.edu>
2024-10-20 15:18:16 -0700

    all tasks done!

--- lab4.py
+++ lab4.py
@@ -1,5 +1,4 @@
 import data
-
 # Write your functions for each part in the space below.
 
 # Part 1
@@ -9,20 +8,39 @@ def first_element(nested_list: list[list[int]]) -> list:
         if len(nested_list[i]) > 0:
             list1.append(nested_list[i][0])
    return list1
-# Purpose: Append the first item within each nested list
+# Purpose: Append the first item to an empty list from each inputted nested list
 # Part 2
-def x_coordinates(self) -> str:
-    return 'Point({}, {})'.format(self.x, self.y)
-
+def x_coordinates(points: list[[list[data.Point]]]) -> list[float]:
+    points1 = []
+    for point in points:
+        points1.append(point.x)
+    return points1
+#Purpose: Append the first item (x-coordinate) to an empty list from each inputted list (point)
 # Part 3
-
-
+def are_in_positive_quadrant(points: list[[list[data.Point]]]) -> list[list]:
+    points2 = []
+    for point in points:
+        if point.x > 0 and point.y > 0:
+            points2.append(point)
+    return points2
+#Purpose: Add the point to an empty list only if the x and y coordinates are positive
 # Part 4
-
-
+def distance(point1: data.Point, point2: data.Point) -> float:
+    euclidean = ((point2.x - point1.x) ** 2 + (point2.y - point1.y) ** 2)**(1/2)
+    return euclidean
+#Purpose: Take the inputted values and find the Euclidean distance
 # Part 5
-
-
+def manhattan_distance(point1: data.Point, point2: data.Point) -> float:
+    manhattan = abs(point1.x - point2.x) - abs(point1.y - point2.y)
+    return manhattan
+#Purpose: Take the inputted values and find the Manhattan distance
 # Part 6
+def distance_all(points: list[data.Point]) -> list[list]:
+    all = []
+    for point in points:
+        euclidean = ((point.x - 0) ** 2 + (point.y - 0) ** 2) ** (1 / 2)
+        all.append(euclidean)
+    return all
+#Purpose: Determine the distance between the origin (0,0) to the inputted points
 
 
--- lab4_tests.py
+++ lab4_tests.py
@@ -19,20 +19,61 @@ class TestCases(unittest.TestCase):
         expected = [2, 4]
         self.assertEqual(expected, result)
     # Part 2
+    def test_x_coordinates_1(self):
+        input = [data.Point(1.0,1.0),data.Point(2.0,3.0),data.Point(4.0,1.0)]
+        result = lab4.x_coordinates(input)
+        expected = [1.0, 2.0, 4.0]
+        self.assertEqual(expected, result)
 
-
+    def test_x_coordinates_2(self):
+        input = [data.Point(7.0, 2.0), data.Point(8.0,9.0), data.Point(1.0,2.0)]
+        result = lab4.x_coordinates(input)
+        expected = [7.0, 8.0, 1.0]
+        self.assertEqual(expected, result)
     # Part 3
-
-
+    def test_are_in_positive_quadrant_1(self):
+        input = [data.Point(1.0, -1.0), data.Point(2.0, 3.0), data.Point(-4.0, 1.0)]
+        result = lab4.are_in_positive_quadrant(input)
+        expected = [data.Point(2.0,3.0)]
+        self.assertEqual(expected, result)
+    def test_are_in_positive_quadrant_2(self):
+        input = [data.Point(-1.0, 1.0), data.Point(-2.0, 3.0), data.Point(4.0, 1.0)]
+        result = lab4.are_in_positive_quadrant(input)
+        expected = [data.Point(4.0,1.0)]
+        self.assertEqual(expected, result)
     # Part 4
-
-
+    def test_distance_1(self):
+        input1, input2 = data.Point(2,3), data.Point(2,6)
+        result = lab4.distance(input1, input2)
+        expected = 3.0
+        self.assertEqual(expected, result)
+    def test_distance_2(self):
+        input1, input2 = data.Point(1,2), data.Point(1,4)
+        result = lab4.distance(input1, input2)
+        expected = 2.0
+        self.assertEqual(expected, result)
     # Part 5
-
-
+    def test_manhattan_distance_1(self):
+        input1, input2 = data.Point(1, 2), data.Point(1, 4)
+        result = lab4.manhattan_distance(input1, input2)
+        expected = -2.0
+        self.assertEqual(expected, result)
+    def test_manhattan_distance_1(self):
+        input1, input2 = data.Point(0, 1), data.Point(2, 3)
+        result = lab4.manhattan_distance(input1, input2)
+        expected = 0.0
+        self.assertEqual(expected, result)
     # Part 6
-
-
+    def test_distance_all_1(self):
+        input = [data.Point(1, 3), data.Point(1,5)]
+        result = lab4.distance_all(input)
+        expected = [3.1622776601683795, 5.0990195135927845]
+        self.assertEqual(expected, result)
+    def test_distance_all_2(self):
+        input = [data.Point(2, 0), data.Point(1,1)]
+        result = lab4.distance_all(input)
+        expected = [2.0, 1.4142135623730951]
+        self.assertEqual(expected, result)
 
 
 
